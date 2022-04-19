481e091424a9660d2af83249a6f9ecbaefb50dc3
priyanka5898 <priyankadhanave5898@gmail.com>
2022-04-19 20:13:52 +0530

    added postgres

---
+++ src/main/java/com/postmancode/postmancode/DatabaseConfig.java
@@ -0,0 +1,24 @@
+package com.postmancode.postmancode;
+
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DatabaseConfig {
+
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
+
+    @Bean
+    public DataSource dataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(dbUrl);
+        return new HikariDataSource(config);
+    }
+}
